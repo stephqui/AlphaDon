0d4e20c047a84711927563829b904cb6e687fd1e
stephqui <paradivuiz@gmail.com>
2022-02-17 17:36:05 +0100

    Maj Idal

--- Alpha/Models/IDal.cs
+++ Alpha/Models/IDal.cs
@@ -9,18 +9,26 @@ namespace Alpha.Models
     {
         void DeleteCreateDatabase();
 
-
-
-        //void GetAllProjects();
-        //void GetMyProject();
-        //void GetDonationsByCollectId();
-        //void AmountCalculation();
+        //*********************************** PROJECT *************************************************
+        void ProjectExiste(string projectName);
+        void GetAllProjects();
+        void GetMyProject();
+        void GetDonationsByCollectId();
+        void AmountCalculation();
         void CreateProject(string projectName, string description, ProjectCategory category, DateTime startDate,
             DateTime endDate, string place, WorldAreas area, Int32 limit, int? profileId, int id, int? collectId);
         void CreateUnitDonation(int id, string payMethod, int CurrentAmount, DateTime date);
+
+
+        //*********************************** PROFILE / USERACCOUNT *************************************************
         int AddUserAccount(string mail, string password);
+
+        void GetAllProfiles();
+        void ProfileChange(int id, string lastName, string firstName, string nationality, Int32 birthday,
+            string nick, string phone, string payMethod);
         UserAccount Authentify(string mail, string password);
         UserAccount GetUserAccount(int id);
+        void GetAllUserAccount();
         UserAccount GetUserAccount(string idStr);
     }
 }
